264ffac09ae5af2b35c0ad63cfb491f4cc5c6967
CSenshi <saba.pochkhua@gmail.com>
2020-04-18 17:36:29 +0400

    divide links into 3 parts

---
+++ DivideLinks.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    arr = [open('SabaLinks.txt', 'w'),
+           open('BeqaLinks.txt', 'w'),
+           open('NikaLinks.txt', 'w'), ]
+
+    with open('links.txt', 'r') as f:
+        links = [line.rstrip() for line in f]
+        totalUsers = len(arr)
+        for i in range(len(links)):
+            arr[i % totalUsers].write('{}\n'.format(links[i]))
